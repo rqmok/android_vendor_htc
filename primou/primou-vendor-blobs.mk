44561904bf6f8cc6560eb4f17a4400e4c64d2e1b
jonas2515 <jonas2515@users.noreply.github.com>
2015-04-26 12:28:06 +0200

    Revert "primou: move wifi firmware to /system/vendor"
    This reverts commit e7332af68670ac6790d148a4db0bcfba410e1d99.
    
    Path is hardcoded in the kernel

--- primou/primou-vendor-blobs.mk
+++ primou/primou-vendor-blobs.mk
@@ -29,11 +29,13 @@ PRODUCT_COPY_FILES += \
 	vendor/htc/primou/proprietary/bin/snd3254:system/bin/snd3254
 
 PRODUCT_COPY_FILES += \
-	vendor/htc/primou/proprietary/vendor/firmware/bcm4330.hcd:system/vendor/firmware/bcm4330.hcd \
-	vendor/htc/primou/proprietary/vendor/firmware/fw_bcm4330_apsta_b1.bin:system/vendor/firmware/fw_bcm4330_apsta_b1.bin \
-	vendor/htc/primou/proprietary/vendor/firmware/fw_bcm4330_apsta_b2.bin:system/vendor/firmware/fw_bcm4330_apsta_b2.bin \
-	vendor/htc/primou/proprietary/vendor/firmware/fw_bcm4330_b2.bin:system/vendor/firmware/fw_bcm4330_b2.bin \
-	vendor/htc/primou/proprietary/vendor/firmware/fw_bcm4330_b1.bin:system/vendor/firmware/fw_bcm4330_b1.bin \
-	vendor/htc/primou/proprietary/vendor/firmware/fw_bcm4330_p2p_b2.bin:system/vendor/firmware/fw_bcm4330_p2p_b2.bin \
-	vendor/htc/primou/proprietary/vendor/firmware/fw_bcm4330_p2p_b1.bin:system/vendor/firmware/fw_bcm4330_p2p_b1.bin
+	vendor/htc/primou/proprietary/etc/firmware/bcm4330.hcd:system/etc/firmware/bcm4330.hcd \
+	vendor/htc/primou/proprietary/etc/firmware/fw_bcm4330_apsta_b1.bin:system/etc/firmware/fw_bcm4330_apsta_b1.bin \
+	vendor/htc/primou/proprietary/etc/firmware/fw_bcm4330_apsta_b2.bin:system/etc/firmware/fw_bcm4330_apsta_b2.bin \
+	vendor/htc/primou/proprietary/etc/firmware/fw_bcm4330_b2.bin:system/etc/firmware/fw_bcm4330_b2.bin \
+	vendor/htc/primou/proprietary/etc/firmware/fw_bcm4330_b1.bin:system/etc/firmware/fw_bcm4330_b1.bin \
+	vendor/htc/primou/proprietary/etc/firmware/fw_bcm4330_p2p_b2.bin:system/etc/firmware/fw_bcm4330_p2p_b2.bin \
+	vendor/htc/primou/proprietary/etc/firmware/fw_bcm4330_p2p_b1.bin:system/etc/firmware/fw_bcm4330_p2p_b1.bin \
+	vendor/htc/primou/proprietary/etc/firmware/calibration:system/etc/calibration
+
 
