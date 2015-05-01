e79b327c762a696678d4b425587c38bcdf2ead27
jonas2515 <jonas2515@users.noreply.github.com>
2015-05-01 15:57:43 +0200

    primou: Clean up * Remove bin directory, most things should be in msm7x30 * Comment out ril-libs

--- primou/primou-vendor-blobs.mk
+++ primou/primou-vendor-blobs.mk
@@ -1,33 +1,24 @@
 # Camera
 PRODUCT_COPY_FILES += \
-	vendor/htc/primou/proprietary/bin/awb_camera:system/bin/awb_camera \
-	vendor/htc/primou/proprietary/bin/lsc_camera:system/bin/lsc_camera \
 	vendor/htc/primou/proprietary/lib/libchromatix_s5k4e5yx_default_video.so:system/lib/libchromatix_s5k4e5yx_default_video.so \
 	vendor/htc/primou/proprietary/lib/libchromatix_s5k4e5yx_preview.so:system/lib/libchromatix_s5k4e5yx_preview.so \
 	vendor/htc/primou/proprietary/lib/libchromatix_s5k4e5yx_hdr.so:system/lib/libchromatix_s5k4e5yx_hdr.so \
 	vendor/htc/primou/proprietary/lib/libchromatix_s5k4e5yx_hfr.so:system/lib/libchromatix_s5k4e5yx_hfr.so \
 	vendor/htc/primou/proprietary/lib/libchromatix_s5k4e5yx_hfr_4x.so:system/lib/libchromatix_s5k4e5yx_hfr_4x.so \
-	vendor/htc/primou/proprietary/lib/libmmcamera_rawchipproc.so:system/lib/libmmcamera_rawchipproc.so \
+	vendor/htc/primou/proprietary/lib/libmmcamera_rawchipproc.so:system/lib/libmmcamera_rawchipproc.so
 
 # Ril
-PRODUCT_COPY_FILES += \
-	vendor/htc/primou/proprietary/lib/libdll.so:system/lib/libdll.so \
-	vendor/htc/primou/proprietary/lib/libdsi_netctrl.so:system/lib/libdsi_netctrl.so \
-	vendor/htc/primou/proprietary/lib/libdsutils.so:system/lib/libdsutils.so \
-	vendor/htc/primou/proprietary/lib/libhtc_ril.so:system/lib/libhtc_ril.so \
-	vendor/htc/primou/proprietary/lib/libidl.so:system/lib/libidl.so \
-	vendor/htc/primou/proprietary/lib/libnetmgr.so:system/lib/libnetmgr.so \
-	vendor/htc/primou/proprietary/lib/libqdp.so:system/lib/libqdp.so \
-	vendor/htc/primou/proprietary/lib/libqmi.so:system/lib/libqmi.so
+#PRODUCT_COPY_FILES += \
+#	vendor/htc/primou/proprietary/lib/libdll.so:system/lib/libdll.so \
+#	vendor/htc/primou/proprietary/lib/libdsi_netctrl.so:system/lib/libdsi_netctrl.so \
+#	vendor/htc/primou/proprietary/lib/libdsutils.so:system/lib/libdsutils.so \
+#	vendor/htc/primou/proprietary/lib/libhtc_ril.so:system/lib/libhtc_ril.so \
+#	vendor/htc/primou/proprietary/lib/libidl.so:system/lib/libidl.so \
+#	vendor/htc/primou/proprietary/lib/libnetmgr.so:system/lib/libnetmgr.so \
+#	vendor/htc/primou/proprietary/lib/libqdp.so:system/lib/libqdp.so \
+#	vendor/htc/primou/proprietary/lib/libqmi.so:system/lib/libqmi.so
 
-# Random Crap..Verify necessity 
-PRODUCT_COPY_FILES += \
-	vendor/htc/primou/proprietary/bin/bma150_usr:system/bin/bma150_usr \
-	vendor/htc/primou/proprietary/bin/logcat2:system/bin/logcat2 \
-	vendor/htc/primou/proprietary/bin/rmt_storage:system/bin/rmt_storage \
-	vendor/htc/primou/proprietary/bin/zcb:system/bin/zcb \
-	vendor/htc/primou/proprietary/bin/snd3254:system/bin/snd3254
-
+# Wifi
 PRODUCT_COPY_FILES += \
 	vendor/htc/primou/proprietary/etc/firmware/bcm4330.hcd:system/etc/firmware/bcm4330.hcd \
 	vendor/htc/primou/proprietary/etc/firmware/fw_bcm4330_apsta_b1.bin:system/etc/firmware/fw_bcm4330_apsta_b1.bin \
