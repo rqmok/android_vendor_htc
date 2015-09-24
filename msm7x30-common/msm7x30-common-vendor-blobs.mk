LegacyXperia/proprietary_vendor_semc

msm7x30: Add radish for IPv6 tethering
Change-Id: I586fd25a91b8ad31650167f56f00ba8c77ce9848

msm7x30-common/msm7x30-common-vendor-blobs.mk
@@ -43,6 +43,7 @@ PRODUCT_COPY_FILES += \
     vendor/semc/msm7x30-common/proprietary/vendor/lib/egl/libGLESv2S3D_adreno.so:system/vendor/lib/egl/libGLESv2S3D_adreno.so \
     vendor/semc/msm7x30-common/proprietary/vendor/lib/egl/libq3dtools_adreno.so:system/vendor/lib/egl/libq3dtools_adreno.so \
     vendor/semc/msm7x30-common/proprietary/lib/libaudioalsa.so:system/lib/libaudioalsa.so \
+    vendor/semc/msm7x30-common/proprietary/bin/radish:system/bin/radish \
     vendor/semc/msm7x30-common/proprietary/lib/libDivxDrm.so:system/lib/libDivxDrm.so \
     vendor/semc/msm7x30-common/proprietary/lib/libExtendedExtractor.so:system/lib/libExtendedExtractor.so \
     vendor/semc/msm7x30-common/proprietary/lib/libI420colorconvert.so:system/lib/libI420colorconvert.so \

rqmok/android_vendor_htc

msm7x30-common/msm7x30-common-vendor-blobs.mk
@@ -22,6 +22,7 @@ PRODUCT_COPY_FILES += \
     vendor/htc/msm7x30-common/proprietary/bin/akmd:system/bin/akmd \
     vendor/htc/msm7x30-common/proprietary/bin/bma150_usr:system/bin/bma150_usr \
     vendor/htc/msm7x30-common/proprietary/bin/lsc_camera:system/bin/lsc_camera \
+    vendor/htc/msm7x30-common/proprietary/bin/radish:system/bin/radish \
     vendor/htc/msm7x30-common/proprietary/bin/rmt_storage:system/bin/rmt_storage \
     vendor/htc/msm7x30-common/proprietary/bin/snd3254:system/bin/snd3254 \
     vendor/htc/msm7x30-common/proprietary/etc/firmware/a225_pfp.fw:system/etc/firmware/a225_pfp.fw \
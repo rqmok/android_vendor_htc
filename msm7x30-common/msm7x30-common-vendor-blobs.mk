c0a16e8181dbc30605269387032e629f456b53f2
Michael Bestas <mikeioannina@gmail.com>
2015-09-17 19:34:54 +0300

    msm7x30: Add radish for IPv6 tethering
    Change-Id: I586fd25a91b8ad31650167f56f00ba8c77ce9848

--- msm7x30-common/msm7x30-common-vendor-blobs.mk
+++ msm7x30-common/msm7x30-common-vendor-blobs.mk
@@ -22,6 +22,7 @@ PRODUCT_COPY_FILES += \
     vendor/htc/msm7x30-common/proprietary/bin/akmd:system/bin/akmd \
     vendor/htc/msm7x30-common/proprietary/bin/bma150_usr:system/bin/bma150_usr \
     vendor/htc/msm7x30-common/proprietary/bin/lsc_camera:system/bin/lsc_camera \
+    vendor/htc/msm7x30-common/proprietary/bin/radish:system/bin/radish \
     vendor/htc/msm7x30-common/proprietary/bin/rmt_storage:system/bin/rmt_storage \
     vendor/htc/msm7x30-common/proprietary/bin/snd3254:system/bin/snd3254 \
     vendor/htc/msm7x30-common/proprietary/etc/firmware/a225_pfp.fw:system/etc/firmware/a225_pfp.fw \
