LegacyXperia/proprietary_vendor_semc

msm7x30: Cleanup proprietary files setup
Change-Id: Ife9d7b73d151b763d49a148f4984a51c3f462a01

msm7x30-common/Android.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2011-2013 The CyanogenMod Project
+# Copyright (C) 2011-2015 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,4 +14,20 @@
 
 # This file is generated by device/semc/msm7x30-common/setup-makefiles.sh
 
-USE_CAMERA_STUB := false
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(filter mogami zeus, $(TARGET_BOOTLOADER_BOARD_NAME)),)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libaudioalsa
+LOCAL_MODULE_OWNER := semc
+LOCAL_SRC_FILES := proprietary/lib/libaudioalsa.so
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_PREBUILT)
+
+endif
+

rqmok/android_vendor_htc

msm7x30-common/Android.mk
@@ -14,7 +14,19 @@
 
 # This file is generated by device/htc/msm7x30-common/setup-makefiles.sh
 
-PRODUCT_PACKAGES += \
-    libaudioalsa
+LOCAL_PATH := $(call my-dir)
 
-$(call inherit-product, vendor/htc/msm7x30-common/msm7x30-common-vendor-blobs.mk)
+ifneq ($(filter primou, $(TARGET_BOOTLOADER_BOARD_NAME)),)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libaudioalsa
+LOCAL_MODULE_OWNER := htc
+LOCAL_SRC_FILES := proprietary/lib/libaudioalsa.so
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_PREBUILT)
+
+endif